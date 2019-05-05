307586608628ad236677b55296897bf3581868dc
swordshine <jianshuo002@gmail.com>
2019-05-05 09:04:04 +0800

    change message

--- 03-nodetest.js
+++ 03-nodetest.js
@@ -1,3 +1,8 @@
 var http=require('http');
 
-http.createServer().listen(3000);
+http.createServer(function(req,res){
+    res.writeHead(200,{"Content-type":"text/plain"});
+    res.end("hello");
+}).listen(3000);
+
+console.log("server start");
